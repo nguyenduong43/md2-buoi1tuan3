f737b8567948a7df01f6d848805a8f7909ae25d9
nguyenduong43 <nguyenduong342003@gmail.com>
2025-12-23 10:40:48 +0700

    add

---
+++ src/LinkedListStack/Test.java
@@ -0,0 +1,10 @@
+package LinkedListStack;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        MyGenericStack.stackOfStrings();
+    }
+}
